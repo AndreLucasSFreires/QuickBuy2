f65ab5d275b6b9f845397b8f67e16736d639f5f0
Andre Lucas <andre.lucas@ed.company>
2020-09-10 22:45:10 -0400

    Criacao das entidades

---
+++ QuickBuy.Dominio/Entidades/ObjetoDoValor/FormaPagamento.cs
@@ -0,0 +1,30 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades.ObjetoDoValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }//todo 4:10 segundos ASP.NET Core Curso
+        public bool EhBoleto 
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool EhNaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
